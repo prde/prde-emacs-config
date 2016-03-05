3fd70daab7001022e2517208bccd6db6215a9bb0
bug2bug <S.Sobko@profitware.ru>
2016-03-05 23:51:13 +0300

    python

---
+++ elpa/ctable-20140304.1659/ctable-autoloads.el
@@ -0,0 +1,15 @@
+;;; ctable-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("ctable.el") (22235 16253 244057 786000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ctable-autoloads.el ends here
