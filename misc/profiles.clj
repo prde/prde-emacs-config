b8b3cce8924b112715f1f6beaeb561611ae54629
Sergey Sobko <SSobko@ptsecurity.com>
2018-01-26 14:05:28 +0300

    cider eldoc

--- misc/profiles.clj
+++ misc/profiles.clj
@@ -1,2 +1,3 @@
 {:repl {:plugins [[cider/cider-nrepl "0.15.1"]]}
- :user {:plugins [[cider/cider-nrepl "0.15.1"]]}}
+ :user {:plugins [[cider/cider-nrepl "0.15.1"]
+                  [lein-nvd "0.4.1"]]}}
